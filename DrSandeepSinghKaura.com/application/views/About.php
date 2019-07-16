4654f0eec61b2986978ca4088f1d76a6303ed00e
VisFunJoy <vishalprofessional2401@gmail.com>
2019-07-16 15:39:14 +0530

    16-07-2019 15:39

--- DrSandeepSinghKaura.com/application/views/About.php
+++ DrSandeepSinghKaura.com/application/views/About.php
@@ -58,27 +58,27 @@
       <h4 class = "sub-heading">Skills, Subjects and Techniques Expertise</h4>
       <div class = "row">
          <div class = "col-xs-12 col-sm-12 col-md-6 col-lg-6">
-            <h6>Skill 1 78%</h6>
+            <h6>Organisational Development (72%)</h6>
             <div class="progress">
-               <div class="progress-bar progress-bar-striped" role="progressbar" style="width: 10%" aria-valuenow="10" aria-valuemin="0" aria-valuemax="100"></div>
+               <div class="progress-bar progress-bar-striped" role="progressbar" style="width: 72%" aria-valuenow="10" aria-valuemin="0" aria-valuemax="100"></div>
             </div>
-            <h6>Skill 1 78%</h6>
+            <h6>Strategising New Business ventures (78%)</h6>
             <div class="progress">
-               <div class="progress-bar progress-bar-striped bg-success" role="progressbar" style="width: 25%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+               <div class="progress-bar progress-bar-striped bg-success" role="progressbar" style="width: 78%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
             </div>
-            <h6>Skill 1 78%</h6>
+            <h6>Enhancing Corporate Interface (75%)</h6>
             <div class="progress">
-               <div class="progress-bar progress-bar-striped bg-info" role="progressbar" style="width: 50%" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
+               <div class="progress-bar progress-bar-striped bg-info" role="progressbar" style="width: 75%" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
             </div>
          </div>
          <div class = "col-xs-12 col-sm-12 col-md-6 col-lg-6">
-            <h6>Skill 1 78%</h6>
+            <h6>Project Planning (87%)</h6>
             <div class="progress">
-               <div class="progress-bar progress-bar-striped bg-warning" role="progressbar" style="width: 75%" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100"></div>
+               <div class="progress-bar progress-bar-striped bg-warning" role="progressbar" style="width: 87%" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100"></div>
             </div>
-            <h6>Skill 1 78%</h6>
+            <h6>Implementation and Foreign Relations (82%)</h6>
             <div class="progress">
-               <div class="progress-bar progress-bar-striped bg-danger" role="progressbar" style="width: 100%" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100"></div>
+               <div class="progress-bar progress-bar-striped bg-danger" role="progressbar" style="width: 82%" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100"></div>
             </div>
          </div>
       </div>
@@ -88,10 +88,12 @@
       <div class = "row">
          <div class = "col-xs-12 col-sm-12 col-md-6 col-lg-6">
             <div class="jumbotron">
-               <h4>Matriculation</h4>
+               <h4>MBA</h4>
                <hr class="my-4">
-               <p class="lead"><b>Session : </b>2018</p>
-               <p class="lead"><b>Result :</b>90%</p>
+               <p class="lead"><b>The Master of Business Administration (MBA) is an 
+                                       internationally-recognized degree designed 
+               to develop the skills required for careers in business and management. 
+               The value of the MBA, however, is not limited strictly to the business world.</b></p>
             </div>
          </div>
          <div style = "display:flex; justify-content: center; align-items: center;" 
@@ -106,20 +108,23 @@
          </div>
          <div class = "col-xs-12 col-sm-12 col-md-6 col-lg-6">
             <div class="jumbotron">
-               <h4>AISSCE</h4>
+               <h4>PhD (H.C)</h4>
                <hr class="my-4">
-               <p class="lead"><b>Session : </b>2018</p>
-               <p class="lead"><b>Result :</b>90%</p>
+               <p class="lead"><b>An honorary doctoral degree is often awarded in recognition 
+               of one's life experiences. It is denoted by the letters "h.c.", 
+               which is an abbreviation for "honoris causa" (Latin for "for the sake of honor"). ... 
+               Honorary doctorates are often awarded by prestigious universities such as Harvard or Oxford.</b></p>
             </div>
          </div>
       </div>
       <div class = "row">
          <div class = "col-xs-12 col-sm-12 col-md-6 col-lg-6">
             <div class="jumbotron">
-               <h4>B.Tech in IT from Thapar University</h4>
+               <h4>Fellowship from University of Oxford</h4>
                <hr class="my-4">
-               <p class="lead"><b>Session : </b>2013 - 2017</p>
-               <p class="lead"><b>Result :</b>90%</p>
+               <p class="lead"><b>The term “Fellow” is commonly used in Oxford to refer to senior 
+               academic and administrative members of a College; 
+               Univ has several different classes of Fellowship.</b></p>
             </div>
          </div>
          <div style = "display:flex; justify-content: center; align-items: center;" 
